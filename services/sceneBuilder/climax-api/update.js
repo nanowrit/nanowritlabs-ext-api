e93e4a88abac2b34a8d469a1ef708a5b97c439ff
Aaron Pennington <aaronpennington@Aarons-MacBook-Pro.local>
2020-04-10 11:31:55 -0600

    first commit

---
+++ services/sceneBuilder/climax-api/update.js
@@ -0,0 +1,40 @@
+import * as dynamoDbLib from "../../../libs/dynamodb-lib";
+import { success, failure } from "../../../libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "ext-climaxs",
+    // 'Key' defines the partition key and sort key of the item to be updated
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'climaxId': path parameter
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      climaxId: event.pathParameters.id,
+    },
+    // 'UpdateExpression' defines the attributes to be updated
+    // 'ExpressionAttributeValues' defines the value in the update expression
+    UpdateExpression: "SET struggle = :struggle, doubt = :doubt, unexpected = :unexpected, climax = :climax, poeticJustice = :poeticJustice, poeticReward = :poeticReward, wrapUp = :wrapUp, attachment = :attachment",
+    ExpressionAttributeValues: {
+      ":struggle": data.struggle || null,
+      ":doubt": data.doubt || null,
+      ":unexpected": data.unexpected || null,
+      ":climax": data.climax || null,
+      ":poeticJustice": data.poeticJustice || null,
+      ":poeticReward": data.poeticReward || null,
+      ":wrapUp": data.wrapUp || null,
+      ":attachment": data.attachment || null,
+    },
+    // 'ReturnValues' specifies if and how to return the item's attributes,
+    // where ALL_NEW returns all attributes of the item after the update; you
+    // can inspect 'result' below to see how it works with different settings
+    ReturnValues: "ALL_NEW"
+  };
+
+  try {
+    await dynamoDbLib.call("update", params);
+    return success({ status: true });
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
